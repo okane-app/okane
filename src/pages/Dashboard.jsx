9c979ce2a31cb17f0e411851f089afce57d07b53
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-22 16:43:15 +0100

    Add Dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { IonContent, IonPage, IonButton } from "@ionic/react";
+import { auth } from "../firebase";
+
+import "../css/pages/Dashboard.css";
+
+function Dashboard() {
+  const [user, setUser] = useState(null);
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUser(user);
+    } else {
+      window.location.href = "./splash";
+    }
+  });
+
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <p>{JSON.stringify(user)}</p>
+      </IonContent>
+    </IonPage>
+  );
+}
+
+export default Dashboard;
