de9088162360d20980a76b59e8bd41840e0dd7ff
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-21 11:13:46 +0100

    Various fixes
    Co-authored-by: WySuKo <WySuKo@users.noreply.github.com>
    Co-authored-by: Laurent HAN <Kuhame@users.noreply.github.com>

--- src/firebase.js
+++ src/firebase.js
@@ -1,84 +1,78 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-import {
-  getFirestore,
-  collection,
-  doc,
-  addDoc,
-  setDoc,
-} from "firebase/firestore";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	sendPasswordResetEmail,
+	signOut,
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYtDHbBmWAjUBGbFV7mwA3be5yofjkwlM",
-  authDomain: "okane-73ae0.firebaseapp.com",
-  projectId: "okane-73ae0",
-  storageBucket: "okane-73ae0.appspot.com",
-  messagingSenderId: "705424858248",
-  appId: "1:705424858248:web:0ed7c800b85ffcafc6133d",
-  measurementId: "G-HTLPNTEQ53",
+	apiKey: "AIzaSyDYtDHbBmWAjUBGbFV7mwA3be5yofjkwlM",
+	authDomain: "okane-73ae0.firebaseapp.com",
+	projectId: "okane-73ae0",
+	storageBucket: "okane-73ae0.appspot.com",
+	messagingSenderId: "705424858248",
+	appId: "1:705424858248:web:0ed7c800b85ffcafc6133d",
+	measurementId: "G-HTLPNTEQ53",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-const login = async (email, password) => {
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // const user = userCredential.user;
-      alert("Connexion réussie !");
-      console.log(auth.currentUser);
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
+const login = (email, password) => {
+	signInWithEmailAndPassword(auth, email, password)
+		// .then((userCredential) => {
+		//   // const user = userCredential.user;
+		//   alert("Connexion réussie !");
+		//   console.log(auth.currentUser);
+		// })
+		.catch((error) => {
+			console.error(error.message);
+		});
 };
 
 const register = async (username, email, password) => {
-  try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const user = res.user;
-    await setDoc(doc(db, "users", user.uid), {
-      username: username,
-      authProvider: "local",
-      email, // email: email
-    });
+	try {
+		const res = await createUserWithEmailAndPassword(auth, email, password);
+		const user = res.user;
+		await setDoc(doc(db, "users", user.uid), {
+			username: username,
+			authProvider: "local",
+			email, // email: email
+		});
 
-    if (user) {
-      console.log(auth.currentUser);
-    }
-  } catch (err) {
-    console.error(err);
-  }
+		if (user) {
+			console.log(auth.currentUser);
+		}
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 const sendPasswordReset = async (email) => {
-  try {
-    await sendPasswordResetEmail(auth, email);
-    alert("Password reset link sent!");
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		await sendPasswordResetEmail(auth, email);
+		alert("Password reset link sent!");
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 const logout = () => {
-  signOut(auth);
+	signOut(auth);
 };
 
 export {
-  auth,
-  db,
-  signInWithEmailAndPassword,
-  login,
-  register,
-  sendPasswordReset,
-  logout,
+	auth,
+	db,
+	signInWithEmailAndPassword,
+	login,
+	register,
+	sendPasswordReset,
+	logout,
 };
