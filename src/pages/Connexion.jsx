17a3411c32edd44d4f94251db131ed063641d728
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-21 11:13:46 +0100

    Various fixes
    Co-authored-by: WySuKo <WySuKo@users.noreply.github.com>
    Co-authored-by: Laurent HAN <Kuhame@users.noreply.github.com>

--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { auth, login } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
@@ -8,57 +8,54 @@ import { hideTabs } from "../utils";
 import "../css/pages/Connexion.css";
 import { IonPage } from "@ionic/react";
 
-function Accueil() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, loading] = useAuthState(auth);
+function Connexion() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [user] = useAuthState(auth);
 
-  useEffect(() => {
-    if (loading) {
-      // maybe trigger a loading screen
-      return;
-    }
-    if (user) <Redirect to="/accueil" />;
-  }, [user, loading]);
+	useEffect(() => {
+		if (user) {
+			console.log("connecté");
+		}
+	}, [user]);
 
-  hideTabs();
+	hideTabs();
 
-  return (
-    <IonPage>
-      <div className="login">
-        <div className="login__container">
-          <input
-            type="text"
-            className="login__textBox"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="E-mail Address"
-          />
-          <input
-            type="password"
-            className="login__textBox"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-          />
-          <button
-            className="login__btn"
-            onClick={async () => {
-              await login(email, password);
-              window.location.href = "./dashboard"; // TODO change
-            }}
-          >
-            Login
-          </button>
-          <div>
-            <Link to="/reset">Forgot Password</Link>
-          </div>
-          <div>
-            Don't have an account? <Link to="/inscription">Register</Link> now.
-          </div>
-        </div>
-      </div>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			<div className="login">
+				<div className="login__container">
+					<input
+						type="text"
+						className="login__textBox"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="E-mail Address"
+					/>
+					<input
+						type="password"
+						className="login__textBox"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Password"
+					/>
+					<button
+						className="login__btn"
+						onClick={async () => {
+							await login(email, password);
+							window.location.href = "./dashboard"; // TODO change
+						}}>
+						Login
+					</button>
+					<div>
+						<Link to="/reset">Forgot Password</Link>
+					</div>
+					<div>
+						Don't have an account? <Link to="/inscription">Register</Link> now.
+					</div>
+				</div>
+			</div>
+		</IonPage>
+	);
 }
-export default Accueil;
+export default Connexion;
