4e2a42b62b12065023312ca31f63fd54b1558b38
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-21 11:44:22 +0100

    Improve login and add logout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,32 @@
 import { useState } from "react";
 import { IonContent, IonPage, IonButton } from "@ionic/react";
-import { auth } from "../firebase";
+import { auth, logout } from "../firebase";
 
 import { showTabs } from "../utils";
 
 import "../css/pages/Dashboard.css";
 
 function Dashboard() {
-  const [user, setUser] = useState(null);
+	const [user, setUser] = useState(null);
 
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      setUser(user);
-    } else {
-      window.location.href = "./splash";
-    }
-  });
+	auth.onAuthStateChanged((user) => {
+		if (user) {
+			setUser(user);
+		} else {
+			window.location.href = "/splash";
+		}
+	});
 
-  showTabs();
+	showTabs();
 
-  return (
-    <IonPage>
-      <IonContent fullscreen>
-        <p>{JSON.stringify(user)}</p>
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			<IonContent fullscreen>
+				<p>{JSON.stringify(user?.email)}</p>
+				<button onClick={() => logout()}>Déconnexion</button>
+			</IonContent>
+		</IonPage>
+	);
 }
 
 export default Dashboard;
