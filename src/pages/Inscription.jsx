4be236192e4deee0027d6dbf99e08196a4fe0a70
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-21 12:02:02 +0100

    Fix login/register

--- src/pages/Inscription.jsx
+++ src/pages/Inscription.jsx
@@ -1,69 +1,89 @@
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
-import { auth, register } from "../firebase";
+import { auth } from "../firebase";
 
 import { hideTabs } from "../utils";
 
 import "../css/pages/Inscription.css";
 import { IonPage } from "@ionic/react";
 
+import { doc, setDoc } from "firebase/firestore";
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { db } from "../firebase";
+
 function Inscription() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-  const [user, loading] = useAuthState(auth);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("");
+	const [user, loading] = useAuthState(auth);
 
-  useEffect(() => {
-    if (loading) return;
-  }, [user, loading]);
+	useEffect(() => {
+		if (loading) return;
+	}, [user, loading]);
 
-  hideTabs();
+	hideTabs();
 
-  return (
-    <IonPage>
-      <div className="register">
-        <div className="register__container">
-          <input
-            type="text"
-            className="register__textBox"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-          />
+	const register = async (username, email, password) => {
+		try {
+			const res = await createUserWithEmailAndPassword(auth, email, password);
+			const user = res.user;
+			await setDoc(doc(db, "users", user.uid), {
+				username: username,
+				authProvider: "local",
+				email, // email: email
+			}).then(() => {
+				window.location.href = "./dashboard"; // TODO change
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-          <input
-            type="text"
-            className="register__textBox"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="E-mail Address"
-          />
+	return (
+		<IonPage>
+			<div className="register">
+				<div className="register__container">
+					<input
+						type="text"
+						className="register__textBox"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						placeholder="Username"
+					/>
 
-          <input
-            type="password"
-            className="register__textBox"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-          />
+					<input
+						type="text"
+						className="register__textBox"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="E-mail Address"
+					/>
 
-          <button
-            className="register__btn"
-            onClick={async () => {
-              await register(username, email, password);
-              window.location.href = "./dashboard"; // TODO change
-            }}
-          >
-            Register
-          </button>
+					<input
+						type="password"
+						className="register__textBox"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Password"
+					/>
 
-          <div>
-            Already have an account? <Link to="/connexion">Login</Link> now.
-          </div>
-        </div>
-      </div>
-    </IonPage>
-  );
+					<button
+						className="register__btn"
+						onClick={() => {
+							register(username, email, password);
+						}}>
+						Register
+					</button>
+
+					<div>
+						Already have an account? <Link to="/connexion">Login</Link> now.
+					</div>
+				</div>
+			</div>
+		</IonPage>
+	);
 }
 export default Inscription;
