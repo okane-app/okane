9565b988a4740689671cc34972c403a5a2dfc74b
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-23 23:39:17 +0100

    Implémente inscription

--- src/routes/GuestStack.jsx
+++ src/routes/GuestStack.jsx
@@ -1,4 +1,5 @@
 import Connexion from "../pages/Connexion";
+import Inscription from "../pages/Inscription";
 import Splash from "../pages/Splash";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -9,6 +10,7 @@ const GuestStack = () => {
 		<Stack.Navigator>
 			<Stack.Screen name="Splash" component={Splash} />
 			<Stack.Screen name="Connexion" component={Connexion} />
+			<Stack.Screen name="Inscription" component={Inscription} />
 		</Stack.Navigator>
 	);
 };
