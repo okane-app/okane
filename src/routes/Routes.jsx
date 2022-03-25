331353909c106b20b2c407567db49523a756eb73
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-25 13:05:06 +0100

    Listen for auth changes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,22 +1,28 @@
 import AuthenticatedTab from "./AuthenticatedTab";
-import GuestStack from "./GuestStack";
 import DepenseTab from "./DepenseTab";
+import GuestStack from "./GuestStack";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
-const Routes = () => {
+const Routes = ({ user }) => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator
 				screenOptions={{
 					headerShown: false,
-				}}
-			>
-				<Stack.Screen name="GuestStack" component={GuestStack} />
-				<Stack.Screen name="UserTab" component={AuthenticatedTab} />
-				<Stack.Screen name="DepenseTab" component={DepenseTab} />
+				}}>
+				{!user && <Stack.Screen name="GuestStack" component={GuestStack} />}
+				{user && (
+					<>
+						<Stack.Screen
+							name="AuthenticatedTab"
+							component={AuthenticatedTab}
+						/>
+						<Stack.Screen name="DepenseTab" component={DepenseTab} />
+					</>
+				)}
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
