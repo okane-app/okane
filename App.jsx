254844cc8fbb54a5c98592abc59ef675ee5240e3
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-25 14:02:17 +0100

    Add message relative timestamp

--- App.jsx
+++ App.jsx
@@ -1,6 +1,10 @@
 import Routes from "./src/routes/Routes";
+import TimeAgo from "javascript-time-ago";
 import { auth } from "./firebase";
+import fr from "javascript-time-ago/locale/fr";
 import { useAuthState } from "react-firebase-hooks/auth";
+
+TimeAgo.addDefaultLocale(fr);
 
 export default function App() {
 	const [user] = useAuthState(auth);
