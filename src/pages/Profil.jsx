fc1b2f3af486fad86f096f598792a7bf280f5c98
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-25 16:11:00 +0100

    Profile page front-end

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -61,21 +61,21 @@ const Profil = () => {
 
 	return (
 		<View style={styles.container}>
-			<View style={{ alignSelf: "center", marginTop: 24 }}>
-				<View style={styles.profileImage}>
-					<Image
-						source={require("../../assets/profile.png")}
-						style={styles.image}></Image>
-				</View>
+			<View style={styles.profileImage}>
+				<Image
+					source={require("../../assets/test.jpg")}
+					style={styles.image}
+					resizeMode="cover"
+				/>
 			</View>
 
-			<View style={styles.infoContainer}>
-				<Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>
+			<View>
+				<Text style={[styles.username, { fontWeight: "200", fontSize: 36 }]}>
 					@{pseudo}
 				</Text>
 			</View>
 
-			<View style={styles.accountMenuBox}>
+			<View style={styles.menu}>
 				{platform === "android" && (
 					<Modal
 						animationType="slide"
@@ -84,65 +84,55 @@ const Profil = () => {
 						onRequestClose={() => {
 							setModalVisible(!modalVisible);
 						}}>
-						<View style={styles.centeredView}>
-							<View style={styles.modalView}>
-								<TextInput
-									style={styles.input}
-									onChangeText={setPseudo}
-									placeholder="Pseudo"
-									value={pseudo}
-								/>
-								<View style={styles.modalButton}>
-									<Pressable
-										style={[styles.button, styles.buttonClose]}
-										onPress={() => {
-											changerPseudo(pseudo);
-											setModalVisible(!modalVisible);
-											setPseudo(pseudo);
-										}}>
-										<Text style={styles.textStyle}>Appliquer</Text>
-									</Pressable>
-									<Pressable
-										style={[styles.button, styles.buttonClose]}
-										onPress={() => setModalVisible(!modalVisible)}>
-										<Text style={styles.textStyle}>Annuler</Text>
-									</Pressable>
-								</View>
+						<View style={styles.modal}>
+							<TextInput
+								style={styles.modalInput}
+								onChangeText={setPseudo}
+								placeholder="Pseudo"
+								value={pseudo}
+							/>
+							<View style={styles.modalButtons}>
+								<Pressable
+									style={[styles.modalButton, styles.modalButtonClose]}
+									onPress={() => {
+										changerPseudo(pseudo);
+										setModalVisible(!modalVisible);
+										setPseudo(pseudo);
+									}}>
+									<Text>Appliquer</Text>
+								</Pressable>
+								<Pressable
+									style={[styles.modalButton, styles.modalButtonClose]}
+									onPress={() => setModalVisible(!modalVisible)}>
+									<Text>Annuler</Text>
+								</Pressable>
 							</View>
 						</View>
 					</Modal>
 				)}
+
 				<TouchableOpacity onPress={() => setModalVisible(true)}>
-					<View style={styles.accountMenuItem}>
-						<View style={styles.accountMenuTextBox}>
-							<Ionicons name="pencil-outline" style={styles.accountMenuIcon} />
-							<Text style={styles.accountMenuText}>Changer de pseudo</Text>
-						</View>
+					<View style={[styles.menuAction, styles.menuActionBorder]}>
+						<Ionicons name="pencil-outline" style={styles.menuIcon} />
+						<Text style={styles.menuText}>Changer de pseudo</Text>
 					</View>
 				</TouchableOpacity>
 
 				<TouchableOpacity>
-					<View style={styles.accountMenuItem}>
-						<View style={styles.accountMenuTextBox}>
-							<Ionicons
-								name="settings-outline"
-								style={styles.accountMenuIcon}
-							/>
-							<Text style={styles.accountMenuText}>Paramètres</Text>
-						</View>
+					<View style={[styles.menuAction, styles.menuActionBorder]}>
+						<Ionicons name="settings-outline" style={styles.menuIcon} />
+						<Text style={styles.menuText}>Paramètres</Text>
 					</View>
 				</TouchableOpacity>
 
 				<TouchableOpacity onPress={logout}>
-					<View style={styles.accountMenuItem}>
-						<View style={styles.accountMenuTextBox}>
-							<Ionicons name="log-out-outline" style={styles.accountMenuIcon} />
-							<Text style={styles.accountMenuText}>Déconnexion</Text>
-						</View>
+					<View style={styles.menuAction}>
+						<Ionicons name="log-out-outline" style={styles.menuIcon} />
+						<Text style={styles.menuText}>Déconnexion</Text>
 					</View>
 				</TouchableOpacity>
 			</View>
-			<StatusBar style="auto" />
+			<StatusBar style="light" />
 		</View>
 	);
 };
@@ -150,56 +140,69 @@ const Profil = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: "#FFF",
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#5BB774",
 	},
-	text: {
-		color: "#52575D",
+
+	username: {
+		marginTop: 10,
+		color: "white",
+		fontWeight: "500",
 	},
+
 	image: {
 		flex: 1,
 		height: undefined,
 		width: undefined,
 	},
+
 	profileImage: {
 		width: 250,
 		height: 250,
-		borderRadius: 100,
+		borderRadius: 250,
 		overflow: "hidden",
 	},
-	infoContainer: {
-		alignSelf: "center",
-		alignItems: "center",
+
+	menu: {
+		marginTop: 35,
+		alignSelf: "stretch",
+		marginLeft: 30,
+		marginRight: 30,
+		paddingTop: 5,
+		paddingBottom: 5,
+		borderWidth: 1,
+		borderRadius: 7,
+		borderColor: "#E8E8E8",
+		backgroundColor: "white",
 	},
-	accountMenuBox: {
-		marginTop: 30,
-	},
-	accountMenuItem: {
-		height: 50,
+
+	menuAction: {
 		flexDirection: "row",
+		alignSelf: "stretch",
 		alignItems: "center",
+		marginLeft: 20,
+		marginRight: 20,
+		paddingTop: 15,
+		paddingBottom: 15,
 	},
-	accountMenuIcon: {
+
+	menuActionBorder: {
+		borderBottomWidth: 1,
+		borderBottomColor: "#E8E8E8",
+	},
+
+	menuIcon: {
 		marginLeft: 15,
 		marginRight: 15,
 	},
-	accountMenuTextBox: {
-		flex: 1,
-		flexDirection: "row",
-		alignItems: "center",
-		paddingLeft: 10,
-		paddingRight: 10,
-	},
-	accountMenuText: {
+
+	menuText: {
 		fontSize: 16,
 		fontWeight: "500",
 	},
-	centeredView: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		marginTop: 22,
-	},
-	modalView: {
+
+	modal: {
 		margin: 20,
 		backgroundColor: "white",
 		borderRadius: 20,
@@ -216,19 +219,23 @@ const styles = StyleSheet.create({
 		shadowRadius: 4,
 		elevation: 5,
 	},
-	modalButton: {
+
+	modalButtons: {
 		alignItems: "center",
 		flexDirection: "row",
 	},
-	button: {
+
+	modalButton: {
 		borderRadius: 20,
 		padding: 10,
 		elevation: 2,
 	},
-	buttonClose: {
+
+	modalButtonClose: {
 		backgroundColor: "#2196F3",
 	},
-	input: {
+
+	modalInput: {
 		height: 40,
 		margin: 12,
 		borderWidth: 1,
