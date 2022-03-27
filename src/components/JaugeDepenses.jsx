372ffeb3c416aea1a4dd52bc397613c5fbfc59e3
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-27 21:52:54 +0200

    Intègre jauge circulaire dans dépenses

---
+++ src/components/JaugeDepenses.jsx
@@ -0,0 +1,18 @@
+import CircularProgress from "react-native-circular-progress-indicator";
+
+const JaugeDepenses = ({ dpt, max, pourcentage }) => (
+	<CircularProgress
+		value={dpt}
+		valueSuffix={`,${(dpt - Math.floor(dpt)).toFixed(2).split(".")[1]} €`}
+		radius={120}
+		duration={2000}
+		textColor={"#ecf0f1"}
+		maxValue={max}
+		title={`${pourcentage}% dépensés`}
+		titleColor={"white"}
+		titleStyle={{ fontSize: 16, fontWeight: "400" }}
+		textStyle={{ fontSize: 40, fontWeight: "500" }}
+	/>
+);
+
+export default JaugeDepenses;
