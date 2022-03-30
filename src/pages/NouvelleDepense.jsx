ef8c044f2be705e6060f480feab84097b37f363d
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-30 12:05:12 +0200

    Reset date on submit

--- src/pages/NouvelleDepense.jsx
+++ src/pages/NouvelleDepense.jsx
@@ -178,6 +178,7 @@ const NouvelleDepense = ({ navigation }) => {
 								setCategorie("");
 								setDepense("");
 								setMontant("");
+								setDate(new Date());
 							});
 						}}>
 						<View style={styles.button}>
